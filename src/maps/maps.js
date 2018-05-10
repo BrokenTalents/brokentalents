92fb376e091f0e35646333b360663deb3f3c0c84
schneefux <schneefux+commit@schneefux.xyz>
2018-05-10 13:47:32 +0200

    build talent name translation map

--- src/maps/maps.js
+++ src/maps/maps.js
@@ -1,18 +1,34 @@
 const rarities = require('./rarities.json');
+const names = require('./names.json');
 
 function getTalentName(talent) {
-  if (talent == 'NoTalent' || talent == '' || talent == undefined) {
+  if (talent == 'NoTalent') {
     return 'No Talent';
   }
 
-  return talent.substring(talent.lastIndexOf('_') + 1, talent.length - 1) // snake case to words
+  switch (talent.slice(talent.length - 7 - 1, talent.length - 1)) {
+    case 'TalentA':
+    case 'TalentB':
+    case 'TalentC':
+      if (Object.keys(names).includes(talent)) {
+        return names[talent];
+      } else {
+        return 'unknown';
+      }
+    default:
+      return talent.substring(talent.lastIndexOf('_') + 1, talent.length - 1) // snake case to words
                .replace(/([A-Z])/g, (s) => ' ' + s.toLowerCase())
                .split(' ') // capitalize first letter
                .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
                .join(' ');
+  }
 }
 
 function getTalentRarity(talent) {
+  if (talent == 'No Talent') {
+    return 'None';
+  }
+
   switch (talent.slice(talent.length - 7 - 1, talent.length - 1)) {
     case 'TalentA': return 'Rare';
     case 'TalentB': return 'Epic';
