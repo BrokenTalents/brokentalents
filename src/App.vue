d4174247609bc3f95fbc7c809a9090458a490269
schneefux <schneefux+commit@schneefux.xyz>
2018-05-11 19:18:24 +0200

    add 'Vainglory stats' to title for SEO

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <div class="hero-body">
         <div class="container">
           <h1 class="title">
-            Broken Talents Fun
+            Vainglory Broken Talents Fun Statistics
           </h1>
           <h2 class="subtitle">
             Tilting Brawlers since May 2017
