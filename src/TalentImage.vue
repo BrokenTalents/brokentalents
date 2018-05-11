890c691b3ee3be52c3e10550c14ca9d204d4d689
schneefux <schneefux+commit@schneefux.xyz>
2018-05-11 22:22:08 +0200

    add hero search box to BR tab

---
+++ src/TalentImage.vue
@@ -0,0 +1,20 @@
+<template>
+  <figure class="image is-64x64">
+    <img :src="'dist/assets/talent-icons-small/' + hero + '/' + hero + '_' + rarity + '.png'" :alt="hero + ' ' + rarity">
+  </figure>
+</template>
+
+<script>
+import Vue from 'vue';
+import * as maps from './maps/maps';
+
+export default Vue.component('talent-image', {
+  props: [ 'entry' ],
+  data: function() {
+    return {
+      hero: maps.getHero(this.entry.Actor),
+      rarity: maps.getTalentRarity(this.entry.Talent),
+    };
+  },
+});
+</script>
