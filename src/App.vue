7d4b849b37bbd180d3588a041e8d0ef9a49cb437
schneefux <schneefux+commit@schneefux.xyz>
2018-05-21 17:03:47 +0200

    Add header image

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container">
-    <section class="hero is-medium is-dark is-bold header">
+    <section class="hero is-medium is-bold header">
       <div class="hero-body">
         <div class="container">
-          <h1 class="title">
+          <h1 class="title has-text-white">
             Broken Talents
           </h1>
-          <h2 class="subtitle">
+          <h2 class="subtitle has-text-white">
             Tilting Brawlers since May 2017
           </h2>
         </div>
@@ -21,5 +21,11 @@
 <style scoped>
 .header {
   text-align: center;
+  background:
+    linear-gradient(
+      rgba(0, 0, 0, 0.4),
+      rgba(0, 0, 0, 0.4)
+    ), url('/dist/assets/TalentGrab.jpg') center center;
+  background-size: cover;
 }
 </style>
