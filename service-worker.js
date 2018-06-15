a4f3e7ffb9f9f8dc769bdc0e2a16d4c8389a7559
schneefux <schneefux+commit@schneefux.xyz>
2018-06-15 14:38:24 +0200

    Add noop service worker

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+/* no-op */
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => {
+  self.clients.matchAll({ type: 'window' }).then((windowClients) => {
+    windowClients.forEach((windowClient) => {
+      windowClient.navigate(windowClient.url);
+    });
+  });
+});
