4aaf25836f44a0ae327e1867a11d76f21fe2ce46
schneefux <schneefux+commit@schneefux.xyz>
2019-02-13 21:15:18 +0100

    enable 4.0 in backend config

--- backend/config.js
+++ backend/config.js
@@ -21,9 +21,9 @@ module.exports = {
     baseUrl: 'https://api.dc01.gamelockerapp.com',
     timeout: 3,
     /* available: 3.1, 3.2, 3.3, … */
-    patchVersion: '3.10',
+    patchVersion: '4.0',
     /* moment js parsable string */
-    startDate: '2019-01-23 18Z',
+    startDate: '2019-02-13 18Z',
     /* available: eu, na, sg, cn, sa, ea */
     regions: ['sg', 'eu', 'na'],
     /* available: casual, 5v5_pvp_casual, ranked, 5v5_pvp_ranked, blitz_pvp_ranked, casual_aral */
